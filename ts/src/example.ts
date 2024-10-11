0caeea60a89658237349624ff66c517f854859af
Tim Runge <tim.runge@wps.de>
2024-10-11 11:29:57 +0200

    board und canvas

--- ts/src/example.ts
+++ ts/src/example.ts
@@ -1,10 +1,51 @@
-function welcome(users: string[]): void {
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-    const sortedUsers: string[] = users.toSorted();
+const blockSize: number = 10;
 
-    sortedUsers.forEach((user: string) => {
-        console.log(`Hello ${user} of length ${user.length}!`);
-    });
+class Board {
+    readonly width: number = 10;
+    readonly height: number = 20;
+
+    readonly state: ('x' | 'o')[][] = [];
+
+    constructor() {
+        this.initializeBoard();
+    }
+
+
+    private initializeBoard(): void {
+        for (let i = 0; i < this.height; i++) {
+            this.state[i] = [];
+            for (let j = 0; j < this.width; j++) {
+                this.state[i][j] = 'o';
+            }
+        }
+    }
+
+    draw(canvas: HTMLCanvasElement):void {
+        canvas.width = this.width * blockSize;
+        canvas.height = this.height * blockSize;
+        const context = canvas.getContext("2d", { alpha: false })!;
+
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
+                if (this.state[i][j] === 'x'){
+                    context.fillStyle = "#03c2ca";
+                    context.fillRect(
+                        j * blockSize, // x
+                        i * blockSize, // y
+                        blockSize, // width
+                        blockSize  // height
+                    );
+                }
+            }
+        }
+    }
 }
 
-welcome(["Brendan", "Douglas", "Anders"]);
+const board = new Board();
+
+board.state[10][5] = 'x';
+
+board.draw(canvas);
+
